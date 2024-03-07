2c72dd7eea74821db7b0ee0afb21db3603230e7a
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-03-07 19:32:10 +0530

    style: format script

--- src/fdtdpy/main.py
+++ src/fdtdpy/main.py
@@ -12,17 +12,20 @@ import visualize
 
 
 def main():
+    """FDTD method"""
 
-	ke = 200
-	ex = np.zeros(ke, dtype=np.float64)
-	hy = np.zeros(ke, dtype=np.float64)
+    ke = 200
+    ex = np.zeros(ke, dtype=np.float64)
+    hy = np.zeros(ke, dtype=np.float64)
 
-	Ex, Hy = sources.pulse(ke, ex, hy)
+    #Ex, Hy = sources.pulse(ke, ex, hy)
+    Ex, Hy = sources.sinusoidal(ke, ex, hy)
 
-	app = QtWidgets.QApplication(sys.argv)
-	window = visualize.MainWindow(Ex, Hy)
-	window.show()
-	sys.exit(app.exec())
+    app = QtWidgets.QApplication(sys.argv)
+    window = visualize.MainWindow(Ex, Hy)
+    window.show()
+    sys.exit(app.exec())
+
 
 if __name__ == "__main__":
-	main()
+    sys.exit(main())
