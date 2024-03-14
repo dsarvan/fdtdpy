a664f7f3b185042e69ea7d4246c90cb0b29bc8b3
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-03-14 19:35:10 +0530

    feat: set the axis labels (position and text) and style parameters

--- src/fdtdpy/visualize.py
+++ src/fdtdpy/visualize.py
@@ -6,6 +6,7 @@ pg.setConfigOptions(antialias=True)
 
 
 class MainWindow(QtWidgets.QMainWindow):
+    """Subclass of QMainWindow to customize application's main window"""
 
     def __init__(self, Ex: np.ndarray, Hy: np.ndarray) -> None:
         super().__init__()
@@ -13,24 +14,39 @@ class MainWindow(QtWidgets.QMainWindow):
         self.Hy: np.ndarray = Hy
         self.index: int = 0
 
+        # set the size parameters (width, height) pixels
         self.setFixedSize(QtCore.QSize(640, 480))
 
+        # set the central widget of the window
         self.graph_widget = pg.GraphicsLayoutWidget(show=True)
         self.setCentralWidget(self.graph_widget)
 
-        self.graph_widget.setBackground("#121317")
+        # set the background color using hex notation #000000 as string
+        self.graph_widget.setBackground("#000000")
 
+        # widget for generating multi-panel figures
         self.graph_line1 = self.graph_widget.addPlot(row=0, col=0)
         self.graph_line2 = self.graph_widget.addPlot(row=1, col=0)
 
+        # set the axis labels (position and text), style parameters
+        styles = {"color": "#dcdcdc", "font-size": "10pt"}
+        self.graph_line1.setLabel("left", "E<sub>x</sub>", **styles)
+        self.graph_line1.setLabel("bottom", " ", **styles)
+        self.graph_line2.setLabel("left", "H<sub>y</sub>", **styles)
+        self.graph_line2.setLabel("bottom", "Spatial Step", **styles)
+
+        # graph_plot method call
         self.graph_plot(self.Ex, self.Hy)
 
     def graph_plot(self, Ex: np.ndarray, Hy: np.ndarray) -> None:
+        """Method accepts Ex and Hy parameters to plot"""
 
+        # set the axis limits within the specified ranges and padding
         self.graph_line1.setXRange(0, Ex.shape[1], padding=0)
         self.graph_line1.setYRange(-1.2, 1.2, padding=0.1)
         self.data_line1 = self.graph_line1.plot(pen="#dcdcdc")
 
+        # set the axis limits within the specified ranges and padding
         self.graph_line2.setXRange(0, Hy.shape[1], padding=0)
         self.graph_line2.setYRange(-1.2, 1.2, padding=0.1)
         self.data_line2 = self.graph_line2.plot(pen="#dcdcdc")
@@ -41,6 +57,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.start()
 
     def update_plot(self) -> None:
+        """Method uses QTimer to update the data every 50 ms"""
         if self.index < self.Ex.shape[0]:
             self.data_line1.setData(self.Ex[self.index])
             self.data_line2.setData(self.Hy[self.index])
